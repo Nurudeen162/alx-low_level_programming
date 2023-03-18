26bb97869e0a19015db305a5744ecc1dd45b1cb3
Nurudeen162 <visitnurudeen@gmail.com>
2023-03-17 19:28:26 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  *  main - Prints a text acording number
  *  Return: Always (Success)
  *
  */
 int main(void)
+
 {
 	int n, lastd;
 
@@ -20,12 +20,12 @@ int main(void)
 		printf("last digit of %d is %d and is greater than 5\n", n, lastd);
 	}
 	else if (lastd == 0)
-	{ 
+	{
 		printf("last digit of %d is %d and is 0\n", n, lastd);
 	}
 	else if (lastd < 6 && lastd != 0)
 	{
-		printf("last digit of %d is %d and is less than 6 and not o\n", n, lastd);
+		printf("last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
 	}
 	return (0);
 }
