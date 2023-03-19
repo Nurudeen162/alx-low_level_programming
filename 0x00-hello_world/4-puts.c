129f5105bb1e3307d03ca0b89f6737643c1040c1
Nurudeen162 <visitnurudeen@gmail.com>
2023-03-19 01:54:19 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that prints followed by new line, use function puts
+ * Return: 0 (success)
+ */
+Int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
