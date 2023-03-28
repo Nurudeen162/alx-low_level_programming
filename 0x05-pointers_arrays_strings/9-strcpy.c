13bbc4fb36176d7b478f54c7fba6f9402a55ece4
Nurudeen162 <visitnurudeen@gmail.com>
2023-03-28 07:11:39 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,35 +1,25 @@
 #include "main.h"
 #include <stdio.h>
-/**
- * _atoi - convert a string to an integer
- * @s: the poniter to convert
- * Return: . A integer
- */
-int _atoi(char *s)
-{
-	int c = 0;
-	unsigned int ni = 0;
-	int min = 1;
-	int isi = 0;
 
-	while (s[c])
+/**
+ * char *_strcpy - a function that copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	int x = 0;
+
+	while (*(src + i) != '\0')
 	{
-		if (s[c] == 45)
-		{
-			min *= -1;
-		}
-		while (s[c] >= 48 && s[c] <= 57)
-		{
-			isi = 1;
-			ni = (ni * 10) + (s[c] - '0');
-			c++;
-		}
-		if (isi == 1)
-		{
-			break;
-		}
-		c++;
+		i++;
 	}
-	ni *= min;
-	return (ni);
+	for (; x < i; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[i] = '\0';
+	return (dest);
 }
