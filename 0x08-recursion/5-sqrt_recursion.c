bce693d37bcaa34083862425158282e4d3589732
Nurudeen162 <visitnurudeen@gmail.com>
2023-04-06 16:27:39 -0700

    6-is_prime_number.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,32 +1,54 @@
 #include "main.h"
+#include <stdio.h>
+int sqrt_helper(int n, int low, int high);
 /**
  * _sqrt_recursion - return the natural square root of a number
  * @n: integer input to find the square root
- * @i: iterate number
- * _sqrt - calculates natural square
  * Return: integer square root of n or -1
  */
-int _sqrt(int n, int i);
-
 int _sqrt_recursion(int n)
 {
-	return (_sqrt(n, 1));
-}
-
-int _sqrt(int n, int i)
-{
-	int sqrt = i * i;
-
-	if (n > 0)
+	if (n < 0)
 	{
 		return (-1);
 	}
 	else if (sqrt == n)
 	{
-		return (1);
+		return (0);
 	}
 	else
 	{
-		return (_sqrt(n, i + 1));
+		return (sqrt_helper(n, 1, n));
+	}
+}
+/**
+ * sqrt_helper -  function for finding the sqaure root recursively
+ * @n: integer input to find the square root
+ * @low: integer lower bound of the search range
+ * @high: integer upper bound of the search range
+ * Return: integer square root of n or -1
+ */
+int sqrt_helper(int n, int low, int high)
+{
+	int mid = (low + high) / 2;
+
+	if (mid * mid == n)
+	{
+		return (mid);
+	}
+	else if (mid * mid < n)
+	{
+		if (mid == low)
+		{
+	return (-1);
+		}
+	else
+	{
+		return (sqrt_helper(n, mid, high));
+	}
+	}
+	else
+	{
+		return (sqrt_helper(n, low, mid));
 	}
 }
