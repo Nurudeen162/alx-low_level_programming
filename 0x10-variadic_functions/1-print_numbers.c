7d3d451be81c7ce15779ad075c765b8d21cb7cb4
Nurudeen162 <visitnurudeen@gmail.com>
2023-04-20 16:43:32 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,6 +1,6 @@
 #include "variadic_functions.h"
 #include <stdio.h>
-#include <stdargs.h>
+#include <stdarg.h>
 /**
  * print_numbers - prints numbes
  * @separator: the strings to be printed between numbers
